1a2fd32d5e2335d33eade7290f4e727b7b455575
Ari Paasonen <paasar@gmail.com>
2022-02-18 14:59:11 +0200

    Create separate handler for new game and join game operations

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,17 @@
 use crate::{ws, Clients, Result};
 use warp::Reply;
 
-pub async fn ws_handler(session: String, ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply> {
-    println!("ws_handler");
+pub async fn new_game_handler(username: String, ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply> {
+    println!("new_game_handler user '{}'", username);
+    let session = username.clone(); // TODO actually generate session
+
+    Ok(ws.on_upgrade(move |socket| ws::client_connection(session.clone(), socket, clients)))
+}
+
+pub async fn join_game_handler(session: String, username :String, ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply> {
+    println!("join_game_handler user '{}' joining to session '{}'", username, session);
+
+    // TODO validate session
 
     Ok(ws.on_upgrade(move |socket| ws::client_connection(session.clone(), socket, clients)))
 }
--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,31 @@ async fn main() {
     let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
 
     println!("Configuring websocket route");
-    let ws_route = warp::path("ws")
+    // ws/new/<username>
+    let ws_route = warp::path("ws");
+
+    let new_route = ws_route
+        .and(warp::path("new"))
         .and(warp::path::param::<String>())
         .and(warp::path::end())
         .and(warp::ws())
         .and(with_clients(clients.clone()))
-        .and_then(handlers::ws_handler);
+        .and_then(handlers::new_game_handler);
 
-    let routes = ws_route.with(warp::cors().allow_any_origin());
+    //ws/join/<session_id>/<username>
+    let join_route = ws_route
+        .and(warp::path("join"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(warp::ws())
+        .and(with_clients(clients.clone()))
+        .and_then(handlers::join_game_handler);
+
+    let routes =
+        new_route
+        .or(join_route)
+        .with(warp::cors().allow_any_origin());
     println!("Starting server");
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
