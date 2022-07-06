47477592a6b136e61896ffe5a123e8b0ff919ec6
Ari Paasonen <paasar@gmail.com>
2022-07-06 15:50:26 +0300

    Add todos to help building game functionality

--- src/main.rs
+++ src/main.rs
@@ -145,4 +145,18 @@ mod tests {
         player_client.send_text("hi from player").await;
         expect_received(&mut host_client, "hi from player").await;
     }
+
+    // TODO #1 join event is delivered
+    // TODO #2 game start chooses word and notifies of roles
+    // TODO #3 hint is stored in state
+    // TODO #4 after last hint, duplicates notification is shown and guesser sees unique hints
+    // TODO #5 guesser's guess is shown
+    // TODO #6 select next guesser and word... -> #2
+
+    // TODO #1.1 trying to join non-existent game gives clear error
+    // TODO #2.1 can't start game with only one player
+    // TODO #5.1 score is updated in state and notified to players
+
+    // TODO #100.1 re-join with existing username
+    // TODO #100.2 heartbeat to drop a player who has lost connection
 }
