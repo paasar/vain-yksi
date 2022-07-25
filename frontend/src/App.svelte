afe6418726f5740e9baa736657a2aff2c56ee816
Ari Paasonen <paasar@gmail.com>
2022-07-25 22:03:00 +0300

    Transition content changes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,19 +4,32 @@
     import Guessing from './components/Guessing.svelte';
     import Lobby from './components/Lobby.svelte';
     import Login from './components/Login.svelte';
+
+    import { fly } from 'svelte/transition';
+
+    let flyIn = {x: 50, duration: 500, delay: 500};
+    let flyOut = {x: -50, duration: 500};
 </script>
 
 <main>
   <h1>Vain Yksi</h1>
   <div class="game-info">
     {#if $game.hints.length > 0 || $game.duplicateHints.length > 0}
-      <Guessing />
+      <div out:fly="{flyOut}" in:fly="{flyIn}">
+        <Guessing />
+      </div>
     {:else if $game.gameStarted}
-      <CollectingHints />
+      <div out:fly="{flyOut}" in:fly="{flyIn}">
+        <CollectingHints />
+      </div>
     {:else if $game.id}
-      <Lobby />
+      <div out:fly="{flyOut}" in:fly="{flyIn}">
+        <Lobby />
+      </div>
     {:else}
-      <Login />
+      <div out:fly="{flyOut}">
+        <Login />
+      </div>
     {/if}
   </div>
 </main>
